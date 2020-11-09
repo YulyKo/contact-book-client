15b121a8b8212813ae8e5b16b626c267f1b4f496
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-09 12:00:12 +0200

    added getRequest to API(all contacts)

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,44 @@
+<template>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Phone</th>
+      <th>Date of Birth</th>
+      <th>Actions</th>
+    </tr>
+    <tr v-for="(id, contact) in conntacts" :key="id">
+      <td>
+        {{ contact.name }}
+      </td>
+      <td>
+        {{ contact.phone }}
+      </td>
+      <td>
+        {{ contact.dateOfBirth }}
+      </td>
+      <td>
+        <button>Edit</button>
+        <button>Delete</button>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters({
+      courses: 'getAllContacts',
+    }),
+  },
+  beforeCreate() {
+    this.$store.dispatch('GET_ALL_CONTACT_FROM_API');
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
+    <contact-list></contact-list>
   </div>
 </template>
 
 <script>
+import ContactsListVue from '@/components/ContactsList.vue';
 // @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {
+    contactList: ContactsListVue,
   },
 };
 </script>
