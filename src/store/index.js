7685e7c3ec784dd9d54bcdb2529acd1be53d541c
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-12 10:20:57 +0200

    adding contact done

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080';
 const GET_ALL_CONTACTS_URL = `${API_URL}/contacts`;
+const POST_CONTACT_URL = `${API_URL}/contacts`;
 
 export default createStore({
   state: {
@@ -15,12 +16,21 @@ export default createStore({
     setAllContacts(state, contacts) {
       state.contacts = contacts;
     },
+    setContactToState(state, contact) {
+      state.contacts.push(contact);
+    },
   },
   actions: {
     GET_ALL_CONTACT_FROM_API({ commit }) {
       axios.get(GET_ALL_CONTACTS_URL)
         .then((resp) => {
           commit('setAllContacts', resp.data);
+        });
+    },
+    POST_CONTACT_TO_API({ commit }, contact) {
+      axios.post(POST_CONTACT_URL, contact)
+        .then(() => {
+          commit('setContactToState', contact);
         });
     },
   },
---
+++ src/views/AddContactPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <adding-form></adding-form>
+</template>
+
+<script>
+import AddingformVue from '@/components/forms/AddingForm.vue';
+
+export default {
+  name: 'AddContactPage',
+  components: {
+    addingForm: AddingformVue,
+  },
+};
+</script>
